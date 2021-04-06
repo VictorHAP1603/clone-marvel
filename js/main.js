2807d6076cb972d506b0b0c1ce796221ce5f917c
VictorHugo1603 <vitor.hugo@newpost.com>
2021-04-06 02:57:20 -0300

    seção news

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 import { InitBanner } from "./modules/banner.js";
-import { CardsGibis } from "./modules/cardsGibis.js";
+import { BoxCards } from "./modules/cardsGibis.js";
 
 const banner = new InitBanner(
   '[data-dropdown="list"]',
@@ -7,7 +7,26 @@ const banner = new InitBanner(
   '[data-dropdown="control"]'
 );
 
+const bannerMobile = () => {
+  const { matches } = matchMedia("(max-width: 852px)");
+
+  if (matches) {
+    banner.addControls();
+    banner.init();
+  } else {
+    banner.removeControl();
+    banner.init();
+  }
+};
+bannerMobile();
+window.addEventListener("resize", bannerMobile);
+banner.changeIten();
+
 banner.init();
 
-const cardGibis = new CardsGibis('[data-card="gibi"]');
+const cardGibis = new BoxCards('[data-card="gibi"]', "../gibis.json");
 cardGibis.init();
+
+const newsCard = new BoxCards('[data-card="latest-news"]', "../news.json");
+newsCard.init();
+newsCard.loadMore();
--- js/modules/banner.js
+++ js/modules/banner.js
@@ -4,6 +4,8 @@ export class InitBanner {
     this.itens = document.querySelectorAll(itens);
     this.controls = document.querySelectorAll(controls);
 
+    this.control = document.querySelectorAll(controls);
+
     this.activeBanner = { active: 0 };
   }
 
@@ -27,6 +29,7 @@ export class InitBanner {
     this.list.style.transform = `translate3d(${
       this.activeBanner.active * -100
     }vw, 0, 0)`;
+
     this.changeIten();
   }
 
@@ -37,9 +40,36 @@ export class InitBanner {
     }, 5000);
   }
 
+  addControls() {
+    const box = document.querySelector(".social-medias");
+    const div = document.createElement("div");
+    const controlsAd = document.querySelector(".controls-added");
+
+    if (!controlsAd) {
+      div.classList.add("controls-added");
+
+      this.itens.forEach(() => {
+        const controlsSpan = document.createElement("span");
+        div.append(controlsSpan);
+      });
+
+      box.parentNode.insertBefore(div, box.previousElementSibling);
+      this.controls = [...div.children];
+    }
+  }
+
+  removeControl() {
+    const controlsAdded = document.querySelector(".controls-added");
+    if (controlsAdded) {
+      controlsAdded.remove();
+    }
+    this.controls = this.control;
+  }
+
   bindEvents() {
     this.handleBanner = this.handleBanner.bind(this);
     this.changeIten = this.changeIten.bind(this);
+    this.removeControl = this.removeControl.bind(this);
   }
 
   init() {
@@ -47,6 +77,5 @@ export class InitBanner {
     this.controls.forEach((control, index) =>
       control.addEventListener("click", (event) => this.handleBanner(index))
     );
-    this.changeIten();
   }
 }
